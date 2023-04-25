f137c4298b670898b2833befc84edfd8466329fd
Talita Reis dos Santos Pereira <talitarsp@outlook.com>
2023-04-24 23:00:26 -0300

    criando modelo de veiculos

---
+++ app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    public class Veiculo
+    {
+    }
+}
